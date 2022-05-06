f8a7b33abefb229b19bfef965e9bb6e5ba3e855a
Tom Ch <74670050+XeoPhantom@users.noreply.github.com>
2022-05-06 12:13:28 +0200

    first

---
+++ inex.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+
+
+class Carte:
+    def __init__(mana,nom, pv):
+        self.mana = mana
+        self.nom = name
+        self.pv = pv
+
+Tuto = Carte(10,"Base", 6)
+
+
+class Mage:
+    def__init__()
